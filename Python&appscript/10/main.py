cf87f59dbc00a22c1e4c696bf69d4543a59b1809
안영진 <anyoungjin0106@kbu.ac.kr>
2024-10-05 21:00:10 +0900

    update main.py

--- Python&appscript/10/main.py
+++ Python&appscript/10/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI,Form,Request
 import hashlib
 import httpx
 import pandas as pd
+from datetime import datetime
 from fastapi.responses import FileResponse
 from fastapi.templating import Jinja2Templates
 import os
@@ -17,12 +18,21 @@ def index():
     return FileResponse("signup.html")
 
 @app.post("/signup")
-async def signup(request:Request,name:str=Form(...),id: str = Form(...),pw: str = Form(...),sex: str = Form(...),date: str = Form(...),ph1: str = Form(...),ph2: str = Form(...),ph3: str = Form(...),m1: str = Form(...),m2: str = Form(...),m3: str = Form(...),m4: str = Form(...)):
+async def signup(request:Request,name:str=Form(...),id: str = Form(...),pw: str = Form(...),sex: str = Form(...),date: datetime = Form(...),ph1: str = Form(...),ph2: str = Form(...),ph3: str = Form(...),m1: str = Form(...),m2: str = Form(...),m3: str = Form(...),m4: str = Form(...)):
     df=getUserTable()
     ph="-".join([ph1,ph2,ph3])
     if len(df[(df['아이디']==id)|(df['전화번호']==ph)])==0:
         pw = hashlib.sha256(pw.encode()).hexdigest()
-        year,month,day=map(int,date.split('-'))
+        print({
+            "method":"insert",
+            "id":id,
+            "pw":pw,
+            "name":name,
+            "sex":sex,
+            "date":date.strftime("%Y-%m-%d"),
+            "ph":ph,
+            "mbti":m1+m2+m3+m4
+        })
         async with httpx.AsyncClient() as client:
             response=await client.post(os.getenv("comedu_system"),data={
             "method":"insert",
@@ -30,9 +40,7 @@ async def signup(request:Request,name:str=Form(...),id: str = Form(...),pw: str 
             "pw":pw,
             "name":name,
             "sex":sex,
-            "year":year,
-            "month":month,
-            "day":day,
+            "date": date.strftime("%Y-%m-%d"),
             "ph":ph,
             "mbti":m1+m2+m3+m4
         })
